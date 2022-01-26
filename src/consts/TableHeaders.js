2589feae2326dec2c5412cbea4a94d6086520f94
VigM <vmikulis@figure.com>
2022-01-25 17:07:57 -0700

    Initial workings

---
+++ src/consts/TableHeaders.js
@@ -0,0 +1,34 @@
+export const TABLE_HEADERS = [
+  {
+    display: 'Download',
+    id: 'download',
+  },
+  {
+    display: 'Network',
+    id: 'network',
+  },
+  {
+    display: 'Checksum',
+    id: 'checksum',
+  },
+  {
+    display: 'Size',
+    id: 'size',
+  },
+  {
+    display: 'Date',
+    id: 'date',
+  },
+  {
+    display: 'Block',
+    id: 'block',
+  },
+  {
+    display: 'Version',
+    id: 'version',
+  },
+  {
+    display: 'Indexed',
+    id: 'indexed',
+  },
+];
---
+++ src/consts/urls.js
@@ -0,0 +1,2 @@
+export const BACKUPS_URL = 'https://storage.googleapis.com/storage/v1/b/provenance-testnet-backups/o';
+export const EXPLORER_URL = 'https://explorer.test.provenance.io';
