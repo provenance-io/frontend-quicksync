709db5b9eb21bb9e8e42eb7d7dafd58dc7fc037d
VigM <vmikulis@figure.com>
2022-03-14 10:40:07 -0600

    update docks to docs

--- src/consts/urls.js
+++ src/consts/urls.js
@@ -1,3 +1,3 @@
 export const BACKUPS_URL = process.env.REACT_APP_BACKUPS_URL;
 export const EXPLORER_URL = process.env.REACT_APP_EXPLORER_URL;
-export const DOCS_URL = process.env.REACT_APP_DOCKS_URL;
+export const DOCS_URL = process.env.REACT_APP_DOCS_URL;
