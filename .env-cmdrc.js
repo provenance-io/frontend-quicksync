ec210ade22fef483b142f6ababf297d34ba71acb
Eric Olson <11235171+eolso@users.noreply.github.com>
2025-02-21 10:18:38 -0800

    Update .env-cmdrc.js

--- .env-cmdrc.js
+++ .env-cmdrc.js
@@ -4,7 +4,7 @@ module.exports = {
     PUBLIC_URL: "/quicksync",
     SKIP_PREFLIGHT_CHECK: "true",
     // API Urls
-    REACT_APP_BACKUPS_URL: "https://storage.googleapis.com/storage/v1/b/provenance-io-test-chain-backup/o/pio-testnet-1%2Fsnapshot-latest.tar.gz",
+    REACT_APP_BACKUPS_URL: "https://storage.googleapis.com/storage/v1/b/provenance-io-test-chain-backup/o/pio-testnet-1%2Fstate-sync-latest.tar.gz",
     REACT_APP_EXPLORER_URL: "https://explorer.test.provenance.io",
     REACT_APP_DOCS_URL: "https://docs.provenance.io/blockchain/running-a-node"
   },
@@ -16,7 +16,7 @@ module.exports = {
     // App settings
     REACT_APP_ENV: "production",
     // API Urls
-    REACT_APP_BACKUPS_URL: "https://storage.googleapis.com/storage/v1/b/provenance-io-chain-backup/o/pio-mainnet-1%2Fsnapshot-latest.tar.gz",
+    REACT_APP_BACKUPS_URL: "https://storage.googleapis.com/storage/v1/b/provenance-io-chain-backup/o/pio-mainnet-1%2Fstate-sync-latest.tar.gz",
     REACT_APP_EXPLORER_URL: "https://explorer.provenance.io",
     REACT_APP_DOCS_URL: "https://docs.provenance.io/blockchain/running-a-node"
   },
